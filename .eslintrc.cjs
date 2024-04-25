58074ef7544d87047ee247abbb1102f6515f8ff2
Mário Henrique <marioh90@gmail.com>
2024-04-25 01:11:18 -0300

    style: atualização de regras de formação

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,12 @@
+const { version } = require('react')
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     // 'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
   ],
@@ -15,11 +18,19 @@ module.exports = {
     project: ['./tsconfig.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': [1], //0 = off, 1 = warn, 2 = error
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
