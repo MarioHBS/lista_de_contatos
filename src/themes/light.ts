2fe161b9f851f07763057df7d52a5450fcc40f99
Mário Henrique <marioh90@gmail.com>
2024-04-25 09:59:14 -0300

    style: estilização

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -1,6 +1,10 @@
 const lightTheme = {
-  lightGreen: '',
+  btnNormal: '#2f3640',
+  redAlert: '#C23616',
+  lightGreen: '#00a86b',
+  verdeHover: '#00cf83',
   darkBlue: '#4476bf',
+  blueEmphasis: '#1e90ff',
 }
 
 export default lightTheme
