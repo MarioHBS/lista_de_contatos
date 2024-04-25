1ce970f5d557346c52b888f44d6f4ef5dc93d450
Mário Henrique <marioh90@gmail.com>
2024-04-25 13:04:16 -0300

    build: atualização de lib

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,9 +5,9 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    // 'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
@@ -20,9 +20,12 @@ module.exports = {
   },
   plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/display-name': [1], //0 = off, 1 = warn, 2 = error
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
