822ba4ff52b30671de6a2160ed3b074b742c7e0b
Mário Henrique <marioh90@gmail.com>
2024-04-26 19:53:11 -0300

    refactor: alterado nome de cor dentro do tema
    > verdeHover -> greenHover

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -2,7 +2,7 @@ const lightTheme = {
   btnNormal: '#2f3640',
   redAlert: '#C23616',
   lightGreen: '#00a86b',
-  verdeHover: '#00cf83',
+  greenHover: '#00cf83',
   darkBlue: '#4476bf',
   blueEmphasis: '#1e90ff',
   backgroundElement: '#fcfcfc',
