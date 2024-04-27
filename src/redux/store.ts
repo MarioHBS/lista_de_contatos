fb9c3b275106d209dd6a0e8e40ed24ca4a7ec7bc
Mário Henrique <marioh90@gmail.com>
2024-04-27 19:18:43 -0300

    refactor: registrado reducer do filtro no Store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import contactReducer from './contacts'
+import filterReducer from './filters/reducer'
 
 const store = configureStore({
   reducer: {
     contact: contactReducer,
+    filters: filterReducer,
   },
 })
 
