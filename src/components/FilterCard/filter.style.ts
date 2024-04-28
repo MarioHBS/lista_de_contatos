052a0fc3cf09ba01c783658c2cedc66f2ea86962
Mário Henrique <marioh90@gmail.com>
2024-04-27 21:08:52 -0300

    refactor: finalizado filtros prontos

--- src/components/FilterCard/filter.style.ts
+++ src/components/FilterCard/filter.style.ts
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
 
 type CardProp = {
-  active: boolean
+  active: string
 }
 
 const StyledCard = styled.div<CardProp>`
   display: inline-block;
   text-align: center;
   padding: 16px 8px;
-  color: ${({ active: selected, theme }) =>
-    selected ? theme.activeCard : theme.unactiveText};
+  color: ${({ active, theme }) =>
+    active === 'true' ? theme.activeCard : theme.unactiveText};
   border: 0.7px solid
     ${({ active: selected, theme }) =>
-      selected ? theme.activeCard : theme.unactiveCard};
+      selected === 'true' ? theme.activeCard : theme.unactiveCard};
   border-radius: 16px;
   font-size: 12px;
   cursor: pointer;
--- src/components/FilterCard/index.tsx
+++ src/components/FilterCard/index.tsx
@@ -1,15 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux'
+
 import StyledCard from './filter.style'
+import { RootReducer } from '../../redux/store'
+import { CategoryType, TypeQuery } from '../../utils/types'
+import { setType } from '../../redux/filters/reducer'
+import { counterSelector } from '../../redux/contacts/contact.selectors'
 
 type FilterProps = {
-  type: 'category' | 'fav' | 'all'
+  type: TypeQuery
   criterion: string
+  value?: CategoryType
+  fav?: boolean
 }
 
-const FilterCard = ({ criterion, type }: FilterProps) => {
+const FilterCard = ({ criterion, fav, type, value }: FilterProps) => {
+  const dsp = useDispatch()
+  const { filters } = useSelector((root: RootReducer) => root)
+
+  const countItems =
+    useSelector( (root: RootReducer) => counterSelector(type, value, root))
+  const setFilter = () => {
+    type === 'fav'
+      ? dsp(setType({ type, fav }))
+      : dsp(setType({ type, value }))
+  }
+
+  const verifyActive = () => filters.type === type && filters.value === value
+
   return (
     <>
-      <StyledCard active={false}>
-        <span>0</span>
+      <StyledCard active={verifyActive().toString()} onClick={setFilter}>
+        <span>{countItems}</span>
         <span>{criterion}</span>
       </StyledCard>
     </>
