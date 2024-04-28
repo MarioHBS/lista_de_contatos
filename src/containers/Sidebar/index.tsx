0f56b3004a3c69d9978bba0b00878863eda6f6a3
Mário Henrique <marioh90@gmail.com>
2024-04-27 21:10:36 -0300

    refactor: adicionando passagem de parâmetro de tipo de categoria para filtragem

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,18 +1,19 @@
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
 import * as SB from './sidebar.style'
 import FilterCard from '../../components/FilterCard'
+import { CategoryType } from '../../utils/types'
 
 const SideBar = () => {
-  const dsp = useDispatch()
+  // const dsp = useDispatch()
 
   return (
     <SB.Aside>
       <SB.Querier type='text' placeholder='Buscar'/>
       {/* Área dos filtros */}
       <SB.FilterContainer>
-        <FilterCard type='category' criterion='família' />
-        <FilterCard type='category' criterion='trabalho' />
-        <FilterCard type='category' criterion='comum' />
+        <FilterCard type='category' criterion='família' value={CategoryType.FAMILY} />
+        <FilterCard type='category' criterion='trabalho' value={CategoryType.WORK} />
+        <FilterCard type='category' criterion='comum' value={CategoryType.COMMON} />
         <FilterCard type='fav' criterion='favorito' />
         <FilterCard type='all' criterion='todos' />
       </SB.FilterContainer>
