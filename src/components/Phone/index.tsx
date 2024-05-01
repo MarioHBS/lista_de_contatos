0aa289c92fcab31e65bb40aacea3f2089d172392
Mário Santos <marioh90@gmail.com>
2024-05-01 17:53:55 -0300

    feat: componente criado: Phone
    > para lidar com números de telefone

--- src/components/Phone/index.tsx
+++ src/components/Phone/index.tsx
@@ -1,40 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { ChangeEvent, useEffect, useState } from 'react'
 
-import styled from 'styled-components'
-import { Entry } from '../../styles/global'
 import { cleanNumber, formatNumber } from '../../utils/tools'
+import { InputPhone, InputPhoneBorder } from './phone.style'
 
 type PhoneType = {
+  border?: boolean
   value: string
   onChange: (phoneNumber: string) => void
 }
 
-const PhoneInput = ({ value, onChange }: PhoneType) => {
+const PhoneInput = ({ border, value, onChange }: PhoneType) => {
   const [phone, setPhone] = useState(value)
 
   useEffect(() => {
     setPhone(formatNumber(value))
   }, [value])
 
-  const InputPhone = styled(Entry)`
-    font-size: 14px;
-    text-transform: none;
-  `
-
   const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
     onChange(cleanNumber(evt.target.value))
   }
 
   return (
-    <InputPhone
-      style={{ fontSize: '14px' }}
-      type="tel"
-      value={phone}
-      onChange={handleChange}
-      placeholder="(xx) xxxx-xxxx"
-      maxLength={16}
-    />
+    <>
+      {!border ? (
+        <InputPhone
+          type="tel"
+          value={phone}
+          onChange={handleChange}
+          placeholder="(xx) xxxx-xxxx"
+          maxLength={16}
+        />
+      ) : (
+        <InputPhoneBorder
+          type="tel"
+          value={phone}
+          onChange={handleChange}
+          placeholder="(xx) xxxx-xxxx"
+          maxLength={16}
+        />
+      )}
+    </>
   )
 }
 
