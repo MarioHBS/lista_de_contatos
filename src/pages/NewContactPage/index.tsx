84afa65a8f163778c3be4644a994b1247016ee0c
Mário Santos <marioh90@gmail.com>
2024-05-01 17:53:05 -0300

    feat: nova página criada, Cadastro

---
+++ src/pages/NewContactPage/index.tsx
@@ -0,0 +1,14 @@
+import RegisterForm from '../../containers/RegisterForm'
+import SideBar from '../../containers/Sidebar'
+
+const NewContactPage = () => {
+  return (
+    <>
+      <SideBar />
+
+      <RegisterForm />
+    </>
+  )
+}
+
+export default NewContactPage
