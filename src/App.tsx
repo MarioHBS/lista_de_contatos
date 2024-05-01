201bd592e9410ddec01e7c9f2caedad3c0c08c3b
Mário Santos <marioh90@gmail.com>
2024-05-01 19:03:36 -0300

    refactor: utilizando router para controle de página

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { ThemeProvider } from 'styled-components'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import GlobalStyle, { GlobalContainer } from './styles/global'
 import HomePage from './pages/HomePage'
 import lightTheme from './themes/light'
 import NewContactPage from './pages/NewContactPage'
 
-const App = () => {
+const pages = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/novo',
+    element: <NewContactPage />
+  }
+])
+
+function App () {
   return (
     <>
       <ThemeProvider theme={lightTheme}>
         <GlobalStyle />
         <GlobalContainer>
-          <NewContactPage />
+          <RouterProvider router={pages} />
         </GlobalContainer>
       </ThemeProvider>
     </>
