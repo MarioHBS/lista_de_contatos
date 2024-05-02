2cdeb96550e9a0b8c3fde1f8295ec1060095e757
Mário Santos <marioh90@gmail.com>
2024-05-01 21:41:24 -0300

    refactor: grande mudança no sistema de salvamento:
    > memória prévia de alteração, para quando cancelar não voltar ao estado zero

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
 import ContactModel from '../../models/Contact'
-import { Button, ButtonSave, Entry } from '../../styles/global'
 import { formatNumber } from '../../utils/tools'
 import { ChangeType } from '../../utils/types'
 import PhoneInput from '../Phone'
+// styles
+import { Button, ButtonSave, Entry } from '../../styles/global'
 import * as Ctt from './contact.style'
 
 type ContactProp = {
@@ -13,13 +14,18 @@ type ContactProp = {
 
 type ButtonEvtType = React.Dispatch<React.SetStateAction<boolean>>
 
-const ButtonArea = (edit: boolean, set: ButtonEvtType) =>
-  edit ? (
+const ButtonArea = (
+  edit: boolean,
+  set: ButtonEvtType,
+  confirm: () => void,
+  reset: () => void,
+) => {
+  return edit ? (
     <>
-      <ButtonSave type="button" onClick={() => set(!edit)}>
+      <ButtonSave type="button" onClick={confirm}>
         Salvar
       </ButtonSave>
-      <Ctt.ButtonCancel>Cancelar</Ctt.ButtonCancel>
+      <Ctt.ButtonCancel onClick={reset}>Cancelar</Ctt.ButtonCancel>
     </>
   ) : (
     <>
@@ -29,34 +35,54 @@ const ButtonArea = (edit: boolean, set: ButtonEvtType) =>
       <Ctt.ButtonCancel>Remover</Ctt.ButtonCancel>
     </>
   )
+}
 
 export const FavComponent = ({ children: aluno }: ContactProp) => {
   const [isEditing, setEdition] = useState(false)
+
+  const [prevName, setPrevName] = useState(aluno.name)
+  const [prevEmail, setPrevEmail] = useState(aluno.email)
+  const [prevTel, setPrevTel] = useState(aluno.phone)
+
   const [name, setName] = useState(aluno.name)
   const [email, setEmail] = useState(aluno.email)
   const [tel, setTel] = useState(aluno.phone)
 
-  const editEmail = ({ target }: ChangeType) => setEmail(target.value)
-  const editName = ({ target }: ChangeType) => setName(target.value)
-  const editTel = ({ target }: ChangeType) => setTel(target.value)
+  const editEmail = ({ target }: ChangeType) => setPrevEmail(target.value)
+  const editName = ({ target }: ChangeType) => setPrevName(target.value)
+  const editTel = (number : string) => setPrevTel(number)
+
+  const confirm = () => {
+    setName(prevName)
+    setEmail(prevEmail)
+    setTel(prevTel)
+    setEdition(false)
+  }
+
+  const reset = () => {
+    setPrevName(name)
+    setPrevEmail(email)
+    setPrevTel(tel)
+    setEdition(false)
+  }
 
   return (
     <Ctt.CardFav edit_status={isEditing.toString()}>
-      {!isEditing ? <h3>{name}</h3> : <Entry type='text' value={name} onChange={editName} />}
+      {!isEditing ? <h3>{name}</h3> : <Entry type='text' value={prevName} onChange={editName} />}
 
       <div style={{ margin: '16px 0' }}>
         {!isEditing
           ? <h4>{email}</h4>
-          : <Ctt.Entry2 type='email' value={email} onChange={editEmail}/>
+          : <Ctt.Entry2 type='email' value={prevEmail} onChange={editEmail}/>
         }
         {!isEditing
           ? <h4>{tel}</h4>
-          : <Ctt.Entry2 type='tel' value={tel} onChange={editTel} />
+          : <PhoneInput value={prevTel} onChange={editTel} />
         }
       </div>
 
       <Ctt.ActionArea>
-        {ButtonArea(isEditing, setEdition)}
+        {ButtonArea(isEditing, setEdition, confirm, reset)}
       </Ctt.ActionArea>
     </Ctt.CardFav>
   )
@@ -64,30 +90,49 @@ export const FavComponent = ({ children: aluno }: ContactProp) => {
 
 export const ContactComponent = ({ children: aluno }: ContactProp) => {
   const [isEditing, setEdition] = useState(false)
+
+  const [prevName, setPrevName] = useState(aluno.name)
+  const [prevEmail, setPrevEmail] = useState(aluno.email)
+  const [prevTel, setPrevTel] = useState(aluno.phone)
+
   const [name, setName] = useState(aluno.name)
   const [email, setEmail] = useState(aluno.email)
   const [tel, setTel] = useState(aluno.phone)
 
-  const editEmail = ({ target }: ChangeType) => setEmail(target.value)
-  const editName = ({ target }: ChangeType) => setName(target.value)
-  const editTel = (number : string) => setTel(number)
+  const editEmail = ({ target }: ChangeType) => setPrevEmail(target.value)
+  const editName = ({ target }: ChangeType) => setPrevName(target.value)
+  const editTel = (number : string) => setPrevTel(number)
+
+  const confirm = () => {
+    setName(prevName)
+    setEmail(prevEmail)
+    setTel(prevTel)
+    setEdition(false)
+  }
+
+  const reset = () => {
+    setPrevName(name)
+    setPrevEmail(email)
+    setPrevTel(tel)
+    setEdition(false)
+  }
 
   return (
     <Ctt.Card edit_status={isEditing.toString()}>
       <div>
-        {!isEditing ? <h3>{name}</h3> : <Entry type='text' value={name} onChange={editName} />}
+        {!isEditing ? <h3>{name}</h3> : <Entry type='text' value={prevName} onChange={editName} />}
 
         {!isEditing
           ? <h4>{email}</h4>
-          : <Ctt.Entry2 type='email' value={email} onChange={editEmail}/>
+          : <Ctt.Entry2 type='email' value={prevEmail} onChange={editEmail}/>
         }
         {!isEditing
           ? <h4 className='tel-field'>{formatNumber(tel)}</h4>
-          : <PhoneInput value={tel} onChange={editTel} />
+          : <PhoneInput value={prevTel} onChange={editTel} />
         }
       </div>
       <Ctt.ActionAreaHz>
-        {ButtonArea(isEditing, setEdition)}
+        {ButtonArea(isEditing, setEdition, confirm, reset)}
       </Ctt.ActionAreaHz>
     </Ctt.Card>
   )
