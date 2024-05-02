933ee3e849b8cecce66f97cbbdfb76ba1dbbfe75
Mário Santos <marioh90@gmail.com>
2024-05-01 23:57:48 -0300

    feat: adicinado funcionamento de retorno textual dos filtros

--- src/containers/ListContatcs/index.tsx
+++ src/containers/ListContatcs/index.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 import { ContactComponent, FavComponent } from '../../components/Contact'
 import { RootReducer } from '../../redux/store'
-import { MainContainer } from '../../styles/global'
+import { MainContainer, TitleResult } from '../../styles/global'
 import Inner from './list.style'
 import { plainContact } from '../../utils/tools'
 
@@ -15,6 +15,21 @@ const ListContacts = () => {
   const { type, query, value } = useSelector(
     (root: RootReducer) => root.filters,
   )
+
+  const displayResultText = (qtt: number) => {
+    let message = ''
+    const tail = query != undefined && query?.length > 0 ? ` e "${query}"` : ''
+
+    if (type === 'all') {
+      message = `${qtt} contato(s) em geral ${tail}`
+    } else if(type === 'fav') {
+      message = `${qtt} contato(s) encontrados como "favorito"${tail}`
+    } else {
+      message = `${qtt} contato(s) encontrados como: "${type} = ${value}"${tail}`
+    }
+
+    return message
+  }
 
   const filtering = () => {
     let filtered = listaContatos
@@ -33,13 +48,16 @@ const ListContacts = () => {
     return filtered
   }
 
+  const filterValue = filtering()
+  const displayValue = displayResultText(filterValue.length)
+
   return (
     <MainContainer>
-      <h2>Título com algum retorno</h2>
+      <TitleResult as="p">{displayValue}</TitleResult>
 
       {favMode ? (
         <Inner.Grid>
-          {filtering().map((item) => (
+          {filterValue.map((item) => (
             <FavComponent key={item.name}>{item}</FavComponent>
           ))}
         </Inner.Grid>
